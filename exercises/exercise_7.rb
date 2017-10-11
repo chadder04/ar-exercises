d0da7e509ec6ccdcce1b561142dcd7e1ceed98af
Chad Garrett <chadgarrett_@hotmail.com>
2017-10-10 17:16:14 -0700

    Completed exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# 2. Ask the user for a store name (store it in a variable)
+prompt_store_name = gets.chomp.to_s
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+create_store = Store.create name: prompt_store_name
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+create_store.errors.messages.each do |e|
+    puts e
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validate :must_have_men_or_womens_apparel
+
+    def must_have_men_or_womens_apparel
+        if (!(mens_apparel || womens_apparel))
+            errors.add(:mens_apparel, "Must have at least a mens_apparel or womens_apparel")
+            errors.add(:womens_apparel, "Must have at least a mens_apparel or womens_apparel")
+        end
+    end
 end
